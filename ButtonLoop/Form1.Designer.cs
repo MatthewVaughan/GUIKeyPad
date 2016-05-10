e6d792184c8ed7055bd801899d83a4eb072257cf
MatthewVaughan <mattman94@gmail.com>
2016-05-10 07:18:08 -0600

    Original Upload

---
+++ ButtonLoop/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace ButtonLoop
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.grpKey = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grpKey.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Georgia", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(34, 52);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(44, 42);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Georgia", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(103, 52);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(44, 42);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Georgia", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(173, 52);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(44, 42);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Georgia", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(34, 113);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(44, 42);
+            this.btn4.TabIndex = 3;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Georgia", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(103, 113);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(44, 42);
+            this.btn5.TabIndex = 4;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Georgia", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(173, 113);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(44, 42);
+            this.btn6.TabIndex = 5;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Georgia", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(34, 184);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(44, 42);
+            this.btn7.TabIndex = 6;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("Georgia", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(103, 184);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(44, 42);
+            this.btn8.TabIndex = 7;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("Georgia", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(173, 184);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(44, 42);
+            this.btn9.TabIndex = 8;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // grpKey
+            // 
+            this.grpKey.Controls.Add(this.button1);
+            this.grpKey.Controls.Add(this.btnClear);
+            this.grpKey.Controls.Add(this.txtNum);
+            this.grpKey.Controls.Add(this.btn5);
+            this.grpKey.Controls.Add(this.btn9);
+            this.grpKey.Controls.Add(this.btn1);
+            this.grpKey.Controls.Add(this.btn8);
+            this.grpKey.Controls.Add(this.btn2);
+            this.grpKey.Controls.Add(this.btn7);
+            this.grpKey.Controls.Add(this.btn3);
+            this.grpKey.Controls.Add(this.btn6);
+            this.grpKey.Controls.Add(this.btn4);
+            this.grpKey.Location = new System.Drawing.Point(97, 99);
+            this.grpKey.Name = "grpKey";
+            this.grpKey.Size = new System.Drawing.Size(250, 390);
+            this.grpKey.TabIndex = 9;
+            this.grpKey.TabStop = false;
+            this.grpKey.Text = "Keypad";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Cambria", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(103, 246);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(44, 42);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "0";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(75, 340);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 32);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtNum
+            // 
+            this.txtNum.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum.Location = new System.Drawing.Point(34, 302);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(183, 32);
+            this.txtNum.TabIndex = 9;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(605, 28);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.aboutToolStripMenuItem.Text = "About...";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 517);
+            this.Controls.Add(this.grpKey);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FrmMain";
+            this.Text = "Keypad";
+            this.grpKey.ResumeLayout(false);
+            this.grpKey.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.GroupBox grpKey;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ ButtonLoop/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// File/Project Prolog
+// Name: Matthew Vaughan
+// Project: Keypad
+// Date: 03/24/2016 11:23:19 am
+//
+// I declare that the following code was written by me. I understand that copying source
+// code from any other source constitutes cheating, and that I will receive
+// a zero on this project if I am found in violation of this policy.
+// ---------------------------------------------------------------------------
+namespace ButtonLoop
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+       
+        }
+        //Declare some constants
+        const int
+            ZERO = 0,
+            ONE = 1,
+            TWO = 2,
+            THREE = 3,
+            FOUR = 4,
+            FIVE = 5,
+            SIX = 6,
+            SEVEN = 7,
+            EIGHT = 8,
+            NINE = 9;
+            
+        /// <summary>
+        /// Btn 1 handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            //Add the number 1 to the number
+            txtNum.Text = ($"{txtNum.Text}{ONE}");
+
+        }
+
+        /// <summary>
+        /// Btn 2 handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            //Add the number 2 to the number
+            txtNum.Text = ($"{txtNum.Text}{TWO}");
+        }
+
+        /// <summary>
+        /// About Handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Display message box showing my credentials
+            MessageBox.Show("Program Master: Matthew Vaughan\n\nHis take on the Keypad.");
+        }
+
+        /// <summary>
+        /// Exit Handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Close application
+            this.Close();
+        }
+
+        /// <summary>
+        /// Btn 3 handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            //Add the number 3 to the number
+            txtNum.Text = ($"{txtNum.Text}{THREE}");
+        }
+
+        /// <summary>
+        /// Btn 4 handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            //Add the number 4 to the number
+            txtNum.Text = ($"{txtNum.Text}{FOUR}");
+        }
+
+        /// <summary>
+        /// Btn 5 handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            //Add the number 5 to the number
+            txtNum.Text = ($"{txtNum.Text}{FIVE}");
+        }
+
+        /// <summary>
+        /// Btn 6 handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            //Add the number 6 to the number
+            txtNum.Text = ($"{txtNum.Text}{SIX}");
+        }
+
+        /// <summary>
+        /// Btn 7 handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            //Add the number 7 to the number
+            txtNum.Text = ($"{txtNum.Text}{SEVEN}");
+        }
+
+        /// <summary>
+        /// Btn 8 handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            //Add the number 8 to the number
+            txtNum.Text = ($"{txtNum.Text}{EIGHT}");
+        }
+
+        /// <summary>
+        /// Btn 9 handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            //Add the number 9 to the number
+            txtNum.Text = ($"{txtNum.Text}{NINE}");
+        }
+
+        /// <summary>
+        /// Btn 0 handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Add the number 0 to the number
+            txtNum.Text = ($"{txtNum.Text}{ZERO}");
+        }
+
+        /// <summary>
+        /// Btn Clear handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //Clear the text box
+            txtNum.Text = "";
+        }
+    }
+}
